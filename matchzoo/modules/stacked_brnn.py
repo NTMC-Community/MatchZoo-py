756b9b0d0289395a05e981d667ca0ff454235df2
Lixin Su <sulixinict@gmail.com>
2019-07-05 22:08:51 +0800

    Add test for stacked rnn

--- matchzoo/modules/stacked_brnn.py
+++ matchzoo/modules/stacked_brnn.py
@@ -10,12 +10,23 @@ class StackedBRNN(nn.Module):
     Differs from standard PyTorch library in that it has the option to save
     and concat the hidden states between layers. (i.e. the output hidden size
     for each sequence input is num_layers * hidden_size).
+
+    Examples:
+        >>> import torch
+        >>> rnn = StackedBRNN(10, 10, 2, concat_layers=True)
+        >>> x = torch.randn(2, 5, 10)
+        >>> x.size()
+        torch.Size([2, 5, 10])
+        >>> x_mask = (torch.ones(2, 5) == 1)
+        >>> rnn(x, x_mask).shape
+        torch.Size([2, 5, 40])
+
     """
 
     def __init__(self, input_size, hidden_size, num_layers,
                  dropout_rate=0, dropout_output=False, rnn_type=nn.LSTM,
                  concat_layers=False, padding=False):
-        """Init."""
+        """Stacked Bidirectional LSTM."""
         super().__init__()
         self.padding = padding
         self.dropout_output = dropout_output
@@ -30,16 +41,7 @@ class StackedBRNN(nn.Module):
                                       bidirectional=True))
 
     def forward(self, x, x_mask):
-        """Encode either padded or non-padded sequences.
-
-        Can choose to either handle or ignore variable length sequences.
-        Always handle padding in eval.
-        Args:
-            x: batch * len * hdim
-            x_mask: batch * len (1 for padding, 0 for true)
-        Output:
-            x_encoded: batch * len * hdim_encoded
-        """
+        """Encode either padded or non-padded sequences."""
         if x_mask.data.sum() == 0:
             # No padding necessary.
             output = self._forward_unpadded(x, x_mask)
