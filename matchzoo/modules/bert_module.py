de859b4d3d83dfe44291f11a2c9e37fb30319169
ruibinxiong <chrisrbxiong@gmail.com>
2019-10-14 02:36:55 -0400

    change token_type_ids +1 -1 cause the bert padding is fixed

--- matchzoo/modules/bert_module.py
+++ matchzoo/modules/bert_module.py
@@ -28,9 +28,8 @@ class BertModule(nn.Module):
         """Forward."""
         input_ids = torch.cat((x, y), dim=-1)
         token_type_ids = torch.cat((
-            torch.zeros(x.shape[0], x.shape[1] + 1),
-            torch.ones(y.shape[0], y.shape[1] - 1)
-        ), dim=-1).long().to(x.device)
+            torch.zeros_like(x),
+            torch.zeros_like(y)), dim=-1).long()
         attention_mask = (input_ids != 0)
         return self.bert(input_ids=input_ids, token_type_ids=token_type_ids,
                          attention_mask=attention_mask)
