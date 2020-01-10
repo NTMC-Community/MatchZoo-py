c29bdf611964de6d9d78ec779df3eaca435bdea9
ruibinxiong <chrisrbxiong@gmail.com>
2020-01-10 04:24:59 -0500

    add matching module test for mask

--- tests/modules/test_modules.py
+++ tests/modules/test_modules.py
@@ -9,7 +9,10 @@ def test_matching():
     y = torch.randn(2, 4, 2)
     z = torch.randn(2, 3, 3)
     for matching_type in ['dot', 'mul', 'plus', 'minus', 'concat']:
+        x_mask = (torch.randint(low=0, high=2, size=(2,3)) == 0)
+        y_mask = (torch.randint(low=0, high=2, size=(2,4)) == 0)
         Matching(matching_type=matching_type)(x, y)
+        Matching(matching_type=matching_type)(x, y, x_mask=x_mask, y_mask=y_mask)
     with pytest.raises(ValueError):
         Matching(matching_type='error')
     with pytest.raises(RuntimeError):
